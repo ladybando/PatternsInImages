0dfd813fb1271f9af13b96b93863feea08d41058
Ayana Bando <ayanaflax@yahoo.com>
2021-12-06 20:44:13 -0500

    initial commit

---
+++ app/src/main/java/com/example/android/patternsinimages/ImageFragment.kt
@@ -0,0 +1,51 @@
+package com.example.android.patternsinimages
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.patternsinimages.databinding.FragmentImageBinding
+
+
+class ImageFragment : Fragment() {
+
+    private var _binding: FragmentImageBinding?= null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentImageBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.cameraButton.setOnClickListener { openCamera() }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == AppCompatActivity.RESULT_OK) {
+            val imageBitmap = data!!.extras!!.get("data") as Bitmap
+            binding.capturedImageView.setImageBitmap(imageBitmap)
+        }
+    }
+
+    private fun openCamera(){
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            "Error message: $e"
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/patternsinimages/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.android.patternsinimages
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.android.patternsinimages.databinding.ActivityMainBinding
+
+const val REQUEST_IMAGE_CAPTURE = 1
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+    }
+}
